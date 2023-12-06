f8cafdeb3a43fb7c4c4c1d53645d9a5e3950a3e2
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2023-12-06 22:30:57 +0300

    dastur yaratildi, birinchi commit

---
+++ kafka-dashboard/src/main/java/uz/kafkadashboard/kafkadashboard/KafkaDashboardApplication.java
@@ -0,0 +1,13 @@
+package uz.kafkadashboard.kafkadashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDashboardApplication.class, args);
+	}
+
+}
---
+++ kafka-dashboard/src/test/java/uz/kafkadashboard/kafkadashboard/KafkaDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.kafkadashboard.kafkadashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
